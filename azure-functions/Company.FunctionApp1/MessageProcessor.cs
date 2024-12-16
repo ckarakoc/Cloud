cb601a8c3d38080df9276a2706c447f9d2b149c9
Celal Karakoç <c.karakoc@live.nl>
2024-12-16 21:30:05 +0100

    Added Azure Functions.

---
+++ azure-functions/Company.FunctionApp1/MessageProcessor.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Company.FunctionApp1;
+
+public class MessageProcessor
+{
+    private readonly ILogger<MessageProcessor> _logger;
+
+    [Function(nameof(MessageProcessor))]
+    public void Run([QueueTrigger("message-queue", Connection = "AzureWebJobsStorage")] 
+        QueueMessage message,
+        ILogger log)
+    {
+        // send email, validate or alert someone
+        _logger.LogInformation($"C# Queue trigger function processed: {message.MessageText}");
+    }
+}
---
+++ azure-functions/Company.FunctionApp1/MessageSender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.FunctionApp1;
+
+public class MessageSender
+{
+    [Function("MessageSender")]
+    public void Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer,
+        ILogger log)
+    {
+        var message = $"C# Timer trigger function executed at: {DateTime.Now}";
+        HttpClient client = new();
+        HttpRequestMessage requestMessage = new(HttpMethod.Post, "https://localhost:7071/api/MessageReceiver");
+        requestMessage.Content = new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json");
+
+        client.Send(requestMessage);
+        log.LogInformation(message);
+    }
+}
